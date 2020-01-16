6a8970e582270536e0f5ee63ff0e07a16735e157
John Nichol <johnny@johns-mbp.home>
2020-01-16 12:54:06 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "Queen";
+let cardTwo = "King";
+let cardThree = "Queen";
+let cardFour = "King";
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
