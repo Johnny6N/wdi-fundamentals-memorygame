0ee3792af8cb0bfb0dca7add54676347aab495a4
John Nichol <johnny@johns-mbp.home>
2020-01-17 09:48:56 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!");
-let cardOne = "Queen";
-let cardTwo = "King";
-let cardThree = "Queen";
-let cardFour = "King";
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+let cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+	console.log('User flipped ' && cardOne);
+console.log('User flipped ' && cardTwo);
+}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else (cardsInPlay[0] != cardsInPlay[1]) 
+		alert('Sorry, try again!')
