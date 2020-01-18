d209509544a1f7ebff222c580e65fbeb536b5e7b
John Nichol <johnny@johns-mbp.home>
2020-01-17 19:02:07 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,20 @@
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
-	console.log('User flipped ' && cardOne);
-console.log('User flipped ' && cardTwo);
-}
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
-	} else (cardsInPlay[0] != cardsInPlay[1]) 
-		alert('Sorry, try again!')
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again!");
+	}
+}
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
+};
+flipCard(0);
+flipCard(2);
